06c60b486a52c6d9586d596190afe1019f4689b6
Shubham Panwar <shubham043panwar@gmail.com>
2024-10-04 23:43:56 +0530

    handlers

--- src/index.js
+++ src/index.js
@@ -6,7 +6,15 @@ dotenv.config({
     path:'./env'
 })
 
-connectDB();
+connectDB()
+.then(()=>{
+    app.listen(process.env.PORT || 8000,()=>{
+        console.log(`Server is running at post :${process.env.PORT}`)
+    })
+})
+.catch((err)=>{
+    console.log('MONGO DB CONNECTION FAILED!!',err)
+})
 
 
 
---
+++ src/utils/ApiResponse.js
@@ -0,0 +1,7 @@
+class ApiResponse{
+    constructor(statusCode,data,message="Success"){
+        this.statusCode = statusCode
+        this.data = message
+        this.success = statusCode < 400
+    }
+}

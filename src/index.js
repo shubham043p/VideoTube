4ca1626ebfc9560e746c7da9a6a560768351e322
Shubham Panwar <shubham043panwar@gmail.com>
2024-10-04 21:27:26 +0530

    database connection

---
+++ src/index.js
@@ -0,0 +1,54 @@
+// require('dotenv').config()
+import dotenv from "dotenv";
+import connectDB from "./db/index.js"
+
+dotenv.config({
+    path:'./env'
+})
+
+connectDB();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import express from "express";
+// const app = express();
+
+
+// ;( async ()=>{
+//     try{
+//         await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`)
+//         app.on("error",(error)=>{
+//             console.log("application not able to talk to db");
+//             throw error
+//         })
+
+//         app.listen(process.env.PORT,()=>{
+//             console.log(`APP IS LISTENING ON PORT ${process.env.PORT}`);
+//         })
+//     } catch(error){
+//         console.log("ERROR",error)
+//         throw err
+//     }
+// })()

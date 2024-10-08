1bcad9a9fe66982c6d12e970c06f86f489b59464
Shubham Panwar <shubham043panwar@gmail.com>
2024-10-08 23:45:05 +0530

    aggregated pipelines

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -260,7 +260,7 @@ const changeCurrentPassword = asyncHandler(async (req,res)=>{
 const getCurrentUser = asyncHandler(async(req,res)=>{
     return res
     .status(200)
-    .json(200,req.user,"Current user fetched successfully")
+    .json(new ApiResponse(200,req.user,"Current user fetched successfully"));
 
 })
 
@@ -272,7 +272,7 @@ const updateAccountDetails = asyncHandler(async (req,res)=>{
         throw new ApiError(400,"All fields are required")
     }
 
-        User.findByIdAndUpdate(
+        const user = await User.findByIdAndUpdate(
             req.user?._id,
             {
                $set:{
@@ -353,6 +353,124 @@ const updateUserCoverImage = asyncHandler(async(req,res)=>{
 })
 
 
+const getUserChannelProfile =  asyncHandler(async(req,res)=>{
+
+    const {username} = req.params
+
+    if(!username?.trim()){
+        throw new ApiError(400,"username is invalid")
+    }
+
+   const channel = await User.aggregate([
+
+    {
+      $match:{
+        username:username?.toLowerCase()
+      }  
+    },
+    {
+        $lookup:{
+        from:"subscriptions",             // Subscription : sbhi field mongodb me save hote time lowercase me ho jati hai aur plural me convert ho jati ha
+        localField:"_id",
+        foreignField:"channel",
+        as:"subscribers"
+    }
+     },
+     {
+        $lookup:{
+            from:"subscriptions",             // Subscription : sbhi field mongodb me save hote time lowercase me ho jati hai aur plural me convert ho jati ha
+            localField:"_id",
+            foreignField:"subscriber",
+            as:"subscribedTo"
+        }
+     },
+     {
+        $addField:{
+            subscribersCount:{
+                $size:"subscribers"
+            },
+            channelsSubcribedToCount:{
+                $size:"subscribedTo"
+            },
+            isSubscribed:{
+                $cond:{
+                    if:{$in:[req.user?._id,"subscribers.subscriber"]},
+                    then:true,
+                    else:false
+                }
+            }
+        }
+     },
+     {
+        $project:{
+            fullName:1,
+            username:1,
+            subscribersCount:1,
+            channelsSubcribedToCount:1,
+            isSubscribed:1,
+            avatar:1,
+            coverImage:1,
+            email:1
+
+        }
+     }
+   ])
+})
+
+const getWatchHistory = asyncHandler (async(req,res)=>{
+       const user = await User.aggregate([
+        {
+            $match:{
+                _id: new mongoose.Types.ObjectId(req.user._id)
+            }
+        },
+        {
+            $lookup:{
+                from:"videos",
+                localField:"watchHistory",
+                foreignField:"_id",
+                as:"watchHistory",
+                pipeline:[
+                    {
+                        $lookup:{
+                            from:"users",
+                            localField:"owner",
+                            foreignField:"_id",
+                            as:"owner",
+                            pipeline:[
+                                {
+                                    $project:{
+                                        fullName:1,
+                                        username:1,
+                                        avatar:1
+                                    }
+                                }
+                            ]
+                        }
+                    },
+                    {
+                        $addField:{
+                            owner:{
+                                $first:"$owner"
+                            }
+                        }
+                    }
+                ]
+            }
+        }
+       ])
+
+     return res
+     .status(200)
+     .json(
+        new ApiResponse(
+            200,
+            user[0].watchHistory,
+            "Watch History fetched successfully"
+        )
+     )  
+})
+
 export {
     registerUser,
     loginUser,
@@ -362,7 +480,9 @@ export {
     getCurrentUser,
     updateAccountDetails,
     updateUserAvatar,
-    updateUserCoverImage
+    updateUserCoverImage,
+    getUserChannelProfile,
+    getWatchHistory
 };
 
 
@@ -396,4 +516,6 @@ user hi baat kr rha hai database se
 user find by id se pata krenge ki user bana ki nahi
 
 ?. optionally unwrap krne ke liye kisi se kuchh select krkr lena
+
+aggregate karne par value arrays ate hai 
 */

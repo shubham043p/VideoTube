cdb2de91d891441189778534ea292eb451f8bd31
Shubham Panwar <shubham043panwar@gmail.com>
2024-10-09 01:15:13 +0530

    models added

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -41,7 +41,7 @@ router.route("/change-password").post(verifyJWT,changeCurrentPassword);
 router.route("/current-user").get(verifyJWT,getCurrentUser);
 router.route("/update-account").patch(verifyJWT,updateAccountDetails);
 router.route("/avatar").patch(verifyJWT,upload.single("avatar"),updateUserAvatar);
-router.route("/cover-image").patch(verifyJWT,upload.single("/coverimage"),updateUserCoverImage);
+router.route("/cover-image").patch(verifyJWT,upload.single("coverimage"),updateUserCoverImage);
 
 
 router.route("/c/:username").get(verifyJWT,getUserChannelProfile);
